bad9703eebc40820d5392e532fe4ee33b16b4ced
SuperFluffyGame <81885757+SuperFluffyGame@users.noreply.github.com>
2022-10-03 19:45:59 -0500

    move to sveltekit

--- src/lib/Board.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-    import type { Board } from "./scripts/board";
-
-    export let board: Board;
-</script>
-
-<div class="board" />
--- src/lib/Cell.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-    import type { Cell } from "./scripts/cell";
-
-    export let cell: Cell;
-</script>
-
-<div class="cell" />
