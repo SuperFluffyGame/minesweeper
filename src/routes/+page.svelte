387840232ee672c092cbc52fe5967ad7b840fa9a
SuperFluffyGame <superfluffygamerminecraft@gmail.com>
2022-10-12 15:53:44 +0000

    new button style?????

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -33,80 +33,45 @@
             {/if}
         {/if}
     </div>
-    <!-- <div slot="content" class="creation-menu">
-        <dialog bind:this={dialog} class="continue-modal">
-            <h2>Are you sure?</h2>
-            <p>
-                You have a game saved, creating a new game will delete the old
-                one.
-            </p>
-            <div class="buttons-container">
-                <button on:click={() => dialog.close()}>Close</button>
-                <button on:click={newGameModalClick}>Continue</button>
-            </div>
-        </dialog>
 
-        <div class="size-select-container">
-            <div class="size-select">
-                <p>8 x 8</p>
-                <p>10 Mines</p>
-                <input
-                    type="radio"
-                    name="size"
-                    id="8"
-                    value={8}
-                    checked
-                    bind:group={selectedBoardSize}
-                />
-            </div>
-            <div class="size-select">
-                <p>12 x 12</p>
-                <p>20 Mines</p>
-                <input
-                    type="radio"
-                    name="size"
-                    id="12"
-                    value={12}
-                    bind:group={selectedBoardSize}
-                />
-            </div>
-            <div class="size-select">
-                <p>16 x 16</p>
-                <p>40 Mines</p>
-                <input
-                    type="radio"
-                    name="size"
-                    id="16"
-                    value={16}
-                    bind:group={selectedBoardSize}
-                />
-            </div>
-        </div>
-
-        <div class="buttons-container">
-            <button
-                class="continue"
-                disabled={!gameSaved}
-                on:click={continueGameClick}>Continue Game</button
-            >
-            <button class="play" on:click={newGameClick}>New Game</button>
-            <button class="custom-game" on:click={customGameClick}
-                >Custom Game</button
-            >
-        </div>
-    </div> -->
-    <div class="content2" slot="content">
+    <div class="content" slot="content">
         <section class="presets">
             <h2>Standard</h2>
+            <!-- <hr /> -->
             <button on:click={newGameClick}>New Game</button>
+            <button class=" border-gradient">Button Testing</button>
+        </section>
+        <section class="custom">
+            <h2>Custom</h2>
         </section>
     </div>
 </SidebarLayout>
 
 <style>
-    .content2 {
-        margin: 1rem;
+    .border-gradient {
+        --shadow-amount: 5px;
+        --push-amount: 3px;
+        border-color: darkcyan;
+        background-color: transparent;
+        color: whitesmoke;
+        box-shadow: 0 var(--shadow-amount) 2px rgba(0, 0, 0, 0.5);
+        transition: transform 100ms ease-in-out, box-shadow 100ms ease-in-out;
     }
+    .border-gradient:hover {
+        background-color: rgba(0, 0, 0, 0.1);
+    }
+    .border-gradient:active {
+        transform: translateY(var(--push-amount));
+        box-shadow: 0 calc(var(--shadow-amount) - var(--push-amount)) 2px
+            rgba(0, 0, 0, 0.5);
+    }
+
+    .content {
+        margin: 1rem;
+        display: flex;
+        flex-direction: column;
+    }
+
     .presets > h2 {
         margin: 0;
     }
@@ -116,55 +81,6 @@
         overflow-y: auto;
     }
 
-    /* .creation-menu {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        flex-grow: 1;
-    }
-
-    .size-select-container {
-        display: flex;
-        flex-direction: row;
-        align-items: center;
-        gap: 2rem;
-    }
-    .size-select {
-        position: relative;
-        width: 8rem;
-        height: 8rem;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-
-        background-color: var(--black2);
-        border-radius: var(--radius);
-
-        font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
-    }
-    .size-select > p {
-        margin: 0.1rem;
-    }
-    .size-select > p:first-child {
-        margin-top: 0.75rem;
-    }
-
-    input[type="radio"] {
-        margin: 0;
-
-        appearance: none;
-        position: absolute;
-        width: 100%;
-        height: 100%;
-        border-radius: var(--radius);
-
-        border-color: var(--black3);
-        transition: border-color 125ms linear;
-        cursor: pointer;
-    }
-    input[type="radio"]:checked {
-        border: 0.25rem solid var(--blue6);
-    } */
     p {
         text-align: center;
     }
